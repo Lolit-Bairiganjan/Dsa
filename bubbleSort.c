9358c5d606ed761e8e8d0ab3238b9847dacbbeb9
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2025-05-22 21:30:06 +0530

    Made bubble sort adaptive

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-void bubbleSort(int array[], int size){
+int bubbleSort(int array[], int size){
     for(int i=0; i<size; i++){
+        int sorted = 1; // Assume sorted
         for(int j=0; j<(size-i-1); j++){
             if(array[j]>array[j+1]){
                 int temp = array[j+1];
                 array[j+1] = array[j];
                 array[j] = temp;
+                sorted = 0; // Found a swap, so not sorted
             }
         }
+        if(sorted){
+            return 1; // Early exit: already sorted
+        }
     }
+    return 0; // Needed full passes
 }
 
 void printElements(int array[], int size){
@@ -20,9 +26,9 @@ void printElements(int array[], int size){
 }
 
 int main(){
-    int array[5] = {2, 1, 5, 11, 9};
+    int array[5] = {5, 4, 3, 2, 1};
     int size = (sizeof(array)/sizeof(int));
-    bubbleSort(array, size);
+    printf("%d\n", bubbleSort(array, size));
     printElements(array, size);
     return 0;
 }
