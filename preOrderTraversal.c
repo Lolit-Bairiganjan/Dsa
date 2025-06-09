28d7c21aa31409d1d8dc9612dc0f42a83f78e996
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2025-06-09 14:56:20 +0530

    Implemented preorder traversal

---
+++ preOrderTraversal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int key;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data){
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    new->key = data;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+}
+
+void preOrder(struct Node* root){
+    if(root==NULL){
+        return;
+    }
+    printf("%d, ", root->key);
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+int main() {
+    struct Node* root = createNode(4);
+    struct Node* p1 = createNode(1);
+    struct Node* p2 = createNode(6);
+    root->left = p1;
+    root->right = p2;
+    struct Node* p1_1 = createNode(5);
+    struct Node* p1_2 = createNode(2);
+    p1->left = p1_1;
+    p1->right = p1_2;
+    preOrder(root);
+    return 0;
+}
