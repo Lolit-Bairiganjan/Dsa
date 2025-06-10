f3b28a46fb261c5b8f9589c05faf6bc2deabb692
Lolit Bairiganjan <wolfrainegaming.you@gmail.com>
2025-06-10 07:04:52 +0530

    Made it actually using an array rather than a modified linked list.

--- stackOperationsArray.c
+++ stackOperationsArray.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Stack{
-    int size;
-    int top;
-    int *stack;
-};
+#define MAX 100
 
-int isEmpty(struct Stack* ptr){
-    if(ptr->top==-1){
+int Stack[MAX];
+int top = -1;
+
+int isEmpty(){
+    if(top==-1){
         return 1;
     }
     return 0;
 }
 
-int isFull(struct Stack* ptr){
-    if(ptr->top==(ptr->size)-1){
+int isFull(){
+    if(top==MAX-1){
         return 1;
     }
     return 0;
 }
 
-int push(struct Stack* ptr, int value){
-    if(isFull(ptr)){
-        printf("Stack Overflow\n");
+void push(int data){
+    if(isFull()){
+        printf("Stack overflow!\n");
+        return;
     }
-    else{
-        ptr->stack[++(ptr->top)] = value;
-    }
+    Stack[++top] = data;
+    printf("Pushed: %d\n", data);
 }
 
-int pop(struct Stack* ptr){
-    if(isEmpty(ptr)){
-        printf("Stack Underflow\n");
-    }
-    else{
-        return (ptr->stack[(ptr->top)--]);
-    }
-}
-
-int peek(struct Stack* ptr, int position){
-    if(isEmpty(ptr)){
-        printf("Stack Empty\n");
+int pop(){
+    if(isEmpty()){
+        printf("Stack Underflow!\n");
         return -1;
     }
-    else{
-        if(position-1>ptr->top){
-            printf("Index not accessible\n");
-            return -1;
-        }
-        printf("The element is: %d\n", ptr->stack[position-1]);
-    }
+    int popped = Stack[top--];
+    printf("Popped: %d\n", popped);
+    return popped;
 }
 
-int main() {
-    struct Stack* stack1 = (struct Stack*)malloc(sizeof(struct Stack));
-    stack1->size = 2;
-    stack1->top = -1;
-    stack1->stack = (int*)malloc(sizeof(int)*stack1->size);
+void peek(){
+    if(isEmpty()){
+        printf("Stack is empty!\n");
+        return;
+    }
+    printf("Top element: %d\n", Stack[top]);
+}
 
-    push(stack1, 10);
-    push(stack1, 20);
-    push(stack1, 30);
-    peek(stack1, 1);
-    peek(stack1, 3);
+int main(){
+    push(10);
+    push(20);
+    push(30);
+    peek();         // Should print 30
+    pop();          // Should remove 30
+    peek();         // Should print 20
+    pop();          // Should remove 20
+    pop();          // Should remove 10
+    pop();          // Should print underflow message
+    peek();         // Should print stack is empty
+    return 0;
 }
